61bd3f7aa3b6dff6ac539b15d09da5039d7b341e
flond7 <elisa.utilivenza@gmail.com>
2022-09-02 12:49:29 +0200

    register customUser to admin interface

--- handleUsers/api/admin.py
+++ handleUsers/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Railway, Question, Video, Result, customUser
+
+# Register your models here.
+# it allows to have a user friendly interface to input data in the db
+
+#admin.site.register(User)
+admin.site.register(customUser)
