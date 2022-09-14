b89a14b12c1379d144973e50fb7984731a17c4b9
flond7 <elisa.utilivenza@gmail.com>
2022-09-14 13:13:13 +0200

    user_overview added

--- handleUsers/api/urls.py
+++ handleUsers/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
   path('', views.apiOverview, name="api-overwiev"),
- ]
+  #path('login', views.index, name="login"),
+  path('user_overview', views.user_overview, name="user_overview"),
+ ] 
--- handleUsers/api/views.py
+++ handleUsers/api/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from django.contrib.auth.decorators import login_required
 from rest_framework.response import Response
+
+from django import template
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.shortcuts import render
+
+from .models import customUser
 
 # Create your views here.
 @api_view(['GET'])
@@ -13,3 +23,16 @@ def apiOverview(request):
     'Delete':'/woman-delete/<str:pk>',
   }
   return Response(basicApi_urls)
+
+""" @login_required(login_url="/login/")
+def index(request):
+    context = {'segment': 'index'}
+
+    return render(request, 'templates/login.html') """
+
+#@login_required(login_url="/login/")
+def user_overview(request):
+    userList = customUser.objects.all()
+    print(userList)
+    return render(request, 'user_overview.html')
+
