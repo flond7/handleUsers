3e5dce0a78d92460486d03919da6dba52b0c046b
flond7 <elisa.utilivenza@gmail.com>
2022-09-14 10:06:32 +0200

    new models

--- handleUsers/api/admin.py
+++ handleUsers/api/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import customUser, officeMail
+from .models import customUser, officeMail, officeSDI, officeAdweb, userAdweb, userEmail, userLan
 
 # Register your models here.
 # it allows to have a user friendly interface to input data in the db
 
 #admin.site.register(User)
 admin.site.register(customUser)
+
 admin.site.register(officeMail)
+admin.site.register(officeAdweb)
+admin.site.register(officeSDI)
+
+admin.site.register(userAdweb)
+admin.site.register(userEmail)
+admin.site.register(userLan)
--- handleUsers/api/models.py
+++ handleUsers/api/models.py
@@ -1,17 +1,72 @@
+import mailbox
 from django.db import models
 import uuid
 from api.modelsConstants import *
 
 # Create your models here.
+
+# Default ids are needed because it is impossible to add a non-nullable field 'adweb' to customuser without specifying a default.
+DEFAULT_MAIL_ID = 1
+DEFAULT_ADWEB_ID = 1
+DEFAULT_LAN_ID = 1
+
+""" 
+- per ogni applicativo ho degli uffici che sono classi officeXXXXXXX
+  (es: officeMail sono le mail uffici, officeAdweb sono gli uffici in adweb...)
+  questo sarà l'elenco a scelta multipla che potrò visualizzare per gli utenti
+- registrare le classi officeXXX nel file admin.py in modo da farle visualizzare nel backend
+- ogni utente ha una classe userXXXXX (es: userMail, userAsweb)
+  che raccoglie tutte le informazioni dell'utente relative a quell'applicazione
+  --- per visualizzare l'elenco a scelta multipla bisogna usare model.ManyToManyFiels(officeXXX)
+
+"""
+
 class officeMail(models.Model):
-  #mail = models.CharField(max_length=100, choices = MAIL_OFFICE_CHOICES)
   mail = models.CharField(max_length=100, blank=True, default='')
-  
-  class Meta:
-    ordering = []
 
   def __str__(self):
     return self.mail
+
+class officeAdweb(models.Model):
+  offices = models.CharField(max_length=100, blank=True, default='')
+
+  def __str__(self):
+    return self.offices
+
+class officeSDI(models.Model):
+  offices = models.CharField(max_length=100, blank=True, default='')
+
+  def __str__(self):
+    return self.offices
+
+
+class userEmail (models.Model):
+  personalMail = models.CharField("Mail personale", max_length=100, blank=True, default='')
+  officeMail = models.ManyToManyField(officeMail)
+
+  def __str__(self):
+    return self.personalMail
+
+class userLan (models.Model):
+  lanId = models.CharField("Nome utente rete locale", max_length=100, blank=True, default='A516-')
+  lanFolders = models.ManyToManyField(officeMail)
+
+  def __str__(self):
+    return self.lanId
+
+class userAdweb (models.Model):
+  adwebId = models.CharField("Nome utente", max_length=100, blank=True, default='A516-')
+  adwebOffices = models.ManyToManyField(officeAdweb)
+
+  def __str__(self):
+    return self.adwebId
+
+class userSDI (models.Model):
+  sdiId = models.CharField("LOGIN FVG", max_length=100, blank=True, default='')
+  sdiOffices = models.ManyToManyField(officeSDI)
+
+  def __str__(self):
+    return self.sdiId
 
 
 class customUser(models.Model):
@@ -22,52 +77,64 @@ class customUser(models.Model):
   cf = models.CharField("Codice fiscale", max_length=100, blank=True, default='')
 
   #MAIL
-  hasMail = models.BooleanField("Ha una mail")
-  personalMail = models.CharField("Mail personale", max_length=100, blank=True, default='')
-  officeMail = models.ManyToManyField(officeMail)
+  mail = models.CharField("Mail personale", max_length=120, blank=False, default="@comune.aviano.pn.it")
+  mailOffices = models.ManyToManyField(officeMail)
+  mailDeleted = models.BooleanField("Mail - Cancellata / disattivata", default=False)
 
   #LAN
-  hasLAN = models.BooleanField()
-  LANid = models.CharField("Nome utente rete locale", max_length=100, blank=True, default='A516-')
+  #lan = models.ForeignKey(userLan, default=DEFAULT_LAN_ID, on_delete=models.CASCADE)
+  lan = models.CharField("ID personale", max_length=120, blank=False, default="A516-")
+  lanDeleted = models.BooleanField("Lan - Cancellata / disattivata", default=False)
 
   #ADWEB
-  hasAdweb = models.BooleanField()
+  adweb = models.ForeignKey(userAdweb, default=DEFAULT_ADWEB_ID, on_delete=models.CASCADE)
+  adwebOffices = models.ManyToManyField(officeAdweb)
+  adwebDeleted = models.BooleanField("Adweb - Cancellato / disattivato", default=False)
   
   #ASCOT
-  hasAscot = models.BooleanField()
+  hasAscot = models.BooleanField(default=False)
 
   #SDI
-  hasSDI = models.BooleanField()
+  hasSDI = models.BooleanField(default=False)
+  sdiOffices = models.ManyToManyField(officeSDI)
+  sdiDeleted = models.BooleanField("SDI - Cancellato / disattivato", default=False)
 
   #ITERATTI
-  hasIteratti = models.BooleanField()
+  hasIteratti = models.BooleanField(default=False)
 
   #BOXAPPS
-  hasBoxapps = models.BooleanField()
+  hasBoxapps = models.BooleanField(default=False)
 
   #WEBSITE
-  hasWebsite = models.BooleanField()
-  websiteUser = models.BooleanField()
-  websiteAdmin = models.BooleanField()
+  hasWebsite = models.BooleanField(default=False)
+  websiteUser = models.BooleanField(default=False)
+  websiteAdmin = models.BooleanField(default=False)
 
   #ITERATTI
-  hasCRM = models.BooleanField()
+  hasCRM = models.BooleanField(default=False)
+  crmDeleted = models.BooleanField("CRM - Cancellato / disattivato", default=False)
 
   #AVCP
-  hasAvcp = models.BooleanField()
+  hasAvcp = models.BooleanField(default=False)
 
   #FVG pay
-  hasFVGPay = models.BooleanField()
+  hasFVGPay = models.BooleanField(default=False)
 
   #SUE
-  hasSue = models.BooleanField()
+  hasSue = models.BooleanField(default=False)
 
   #SUAP
   #hasSuap = models.BooleanField()
 
   #SERVIZI SCOLASTICI - Portale Kpax
-  hasSercScuola = models.BooleanField()
+  hasServScuola = models.BooleanField(default=False)
+  servScuolaDeleted = models.BooleanField("Servizi scolastici - Cancellato / disattivato", default=False)
 
+  #ALBO PRETORIO
+  hasAlboPret = models.BooleanField(default=False)
+  alboPretDeleted = models.BooleanField("Albo pretorio - Cancellato / disattivato", default=False)
+
+  note = models.TextField(default="")
 
   class Meta:
     ordering = []
