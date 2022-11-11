e4e92c63638e01f9b106460bfdcd03c3e473ca35
flond7 <elisa.utilivenza@gmail.com>
2022-11-11 08:32:03 +0100

    added note fields

--- handleUsers/api/models.py
+++ handleUsers/api/models.py
@@ -90,8 +90,8 @@ class customUser(models.Model):
   #LAN
   #lan = models.ForeignKey(userLan, default=DEFAULT_LAN_ID, on_delete=models.CASCADE)
   lan = models.CharField("ID personale", max_length=120, blank=False, default="A516-")
-  lanRole = models.CharField("Ruolo in LAN", max_length = 2, choices = LAN_ROLES_CHOICES, default = 'l1', blank=False)
   lanOffice = MultiSelectField(max_length = 100, choices = LAN_OFFICE_CHOICES, default = 'mo0', blank=False)
+  lanRole = models.CharField("Ruolo in LAN", max_length = 2, choices = LAN_ROLES_CHOICES, default = 'l1', blank=False)
   lanNote = models.CharField("Note per lan", max_length = 150, default='', blank=True)
   
   #ADWEB
@@ -100,17 +100,19 @@ class customUser(models.Model):
   adwebNote = models.CharField("Note per adweb", max_length = 150, default='', blank=True)
     
   #ASCOT
-  ascotRole = models.CharField("Ruolo Ascot", max_length = 4, choices = ASCOT_ROLES_CHOICES, default = 'a0', blank=False)
-  ascotOffice = MultiSelectField("Uffici Ascot", max_length = 100, choices = ASCOT_OFFICE_CHOICES, default = 'a0', blank=False)
+  ascotOffice = MultiSelectField("Uffici ascot", max_length = 100, choices = ASCOT_OFFICE_CHOICES, default = 'a0', blank=False)
+  ascotRole = models.CharField("Ruolo ascot", max_length = 4, choices = ASCOT_ROLES_CHOICES, default = 'a0', blank=False)
+  ascotNote = models.CharField("Note per ascot", max_length = 150, default='', blank=True)
 
   #SDI
-  sdiRole = models.CharField("Ruolo in SDI", max_length = 4, choices = SDI_ROLES_CHOICES, default = 'sdi0', blank=False)
   sdiOffice = MultiSelectField("Uffici SDI", max_length = 100, choices = SDI_OFFICE_CHOICES, default = 'sdi0', blank=False)
-  
+  sdiRole = models.CharField("Ruolo in SDI", max_length = 4, choices = SDI_ROLES_CHOICES, default = 'sdi0', blank=False)
+  sdiNote = models.CharField("Note per SDI", max_length = 150, default='', blank=True)
+
   #GIFRA - ITERATTI
   iteratti = models.CharField("ID iteratti", max_length=120, blank=True, default="")
-  iterattiRole = models.CharField(max_length = 2, choices = ITERATTI_ROLES_CHOICES, default = 'i0', blank=False)
   iterattiOffice = MultiSelectField("Uffici iteratti", max_length = 100, choices = ITERATTI_OFFICE_CHOICES, default = 'i0', blank=False)
+  iterattiRole = models.CharField(max_length = 2, choices = ITERATTI_ROLES_CHOICES, default = 'i0', blank=False)
   iterattiNote = models.CharField("Note per iteratti", max_length = 150, default='', blank=True)
 
   #BOXAPPS
@@ -157,7 +159,7 @@ class customUser(models.Model):
   websiteDeleted = models.BooleanField("Sito disattivato", default=False)
   crmDeleted = models.BooleanField("CRM disattivato", default=False)
   avcpDeleted = models.BooleanField("AVCP disattivato", default=False)
-  servScuolaDeleted = models.BooleanField("Servizi scolastici disattivato", default=False)
+  servScuolDeleted = models.BooleanField("Servizi scolastici disattivato", default=False)
   alboPretDeleted = models.BooleanField("Albo pretorio disattivato", default=False)
   masterdataDeleted = models.BooleanField("Masterdata eliminato", default=False)
 
