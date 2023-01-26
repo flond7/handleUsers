7b338c81859100bdc92b6446e801903158beedf3
flond7 <elisa.utilivenza@gmail.com>
2023-01-26 12:43:29 +0100

    added users status filters

--- handleUsers/api/filters.py
+++ handleUsers/api/filters.py
@@ -12,4 +12,4 @@ class customUserFilter(django_filters.FilterSet):
 
   class Meta:
     model = customUser
-    fields = {'name': ['exact'], 'surname': ['exact'], 'office': ['exact']}
+    fields = {'name': ['exact'], 'surname': ['exact'], 'office': ['exact'], 'active':['exact'], 'employed': ['exact']}
